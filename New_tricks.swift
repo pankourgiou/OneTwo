9b95699757395999bd75e358f4dca03558995252
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 09:51:01 +0200

    Create New_tricks.swift

---
+++ New_tricks.swift
@@ -0,0 +1,6 @@
+//New tricks
+print("Not all psychologers wa-wa-want to pu-pu-put to-to-to-xic people aside")//1
+print("There is pop a.i. and po-po-pop psychology...psychology and a.i. 4-4 the ma-ma-masses")//2
+print("Do you-you-you find this fu-fu-funny? it is a bi-bi-bit funny but it-it-it is serious 2")//3
+print("we-we-we will hire the-the best")//4
+//I lo-lo-love to-to-to-xic people because they are not toxic
